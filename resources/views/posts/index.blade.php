e0b0cbed674ae1de4583e0e93e1fdb1fee903841
Alice <amartinbusiness@outlook.com>
2026-02-23 18:03:14 +0000

    Initial commit

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Blog</title>
+</head>
+<body>
+    <h1>My personal Blog</h1>
+
+    @if ($posts->count())
+        <ul>
+            @foreach ($posts as $post)
+
+            <li>
+                <h2>
+                <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h2>
+                <div>{{ $post->excerpt }}</div>
+                <div>{{ $post->author }} / {{ $post->date->diffForHumans() }}</div>
+            </li>
+                        
+            @endforeach
+        </ul>
+    @endif
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Spatie\Sheets\Facades\Sheets;
+
+Route::get('/', function () {
+    $posts = Sheets::collection('posts')->all();
+
+     return view('posts.index', [
+        'posts' => $posts
+     ]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $post = Sheets::collection('posts')->all()->where('slug', $slug);
+// dd($post);
+    abort_if(is_null($post), 404);
+
+    return view('posts.show', ['post' => $post]);
+});
+
+Route::view('/test', 'app');
